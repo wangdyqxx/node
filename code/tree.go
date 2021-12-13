41c5edc3cef4fd055f20e28c28597a43d4c254c3
dongyang <wangdongyang3239@ipalfish.com>
2021-12-13 12:48:31 +0800

    1

--- code/tree.go
+++ code/tree.go
@@ -295,30 +295,6 @@ func (this *Codec) deserialize(data string) *TreeNode {
 }
 
 //层序遍历二叉树
-func levelOrder111(root *TreeNode) [][]int {
-	ret := [][]int{}
-	if root == nil {
-		return ret
-	}
-	q := []*TreeNode{root}
-	for i := 0; len(q) > 0; i++ {
-		ret = append(ret, []int{})
-		p := []*TreeNode{}
-		for j := 0; j < len(q); j++ {
-			node := q[j]
-			ret[i] = append(ret[i], node.Val)
-			if node.Left != nil {
-				p = append(p, node.Left)
-			}
-			if node.Right != nil {
-				p = append(p, node.Right)
-			}
-		}
-		q = p
-	}
-	return ret
-}
-
 func levelOrder(root *TreeNode) [][]int {
 	var arrres [][]int
 	if root == nil {
